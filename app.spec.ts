a7aea86fffed0386d82b37b9cd7b52a016ea4a98
pkosiniak <37831217+pkosiniak@users.noreply.github.com>
2019-01-09 17:21:06 +0100

    ...

--- app.spec.ts
+++
@@ -1,9 +0,0 @@
-import agent from 'supertest';
-import app from './app';
-
-describe('root request', () => {
-   it('should return 404', async () => {
-      const response = await agent(app).get('/');
-      expect(response.status).toBe(404);
-   });
-});
